9a329648fc93c7b0963a7f408b4c778d403da56d
Bartek Łasocha <107810474+Student25876@users.noreply.github.com>
2024-06-29 01:22:57 +0400

    Update ReuseMagisterCoin.sol

--- Case4/ReuseMagisterCoin.sol
+++ Case4/ReuseMagisterCoin.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MyContract {
+    IERC20 public magisterCoin;
+
+    constructor(address _tokenAddress) {
+        magisterCoin = IERC20(_tokenAddress);
+    }
+
+    function getBalance(address account) external view returns (uint256) {
+        return magisterCoin.balanceOf(account);
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(magisterCoin.transferFrom(msg.sender, to, amount), "Transfer failed");
+    }
+
+    function approve(address spender, uint256 amount) external {
+        require(magisterCoin.approve(spender, amount), "Approval failed");
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return magisterCoin.allowance(owner, spender);
+    }
+}
 
